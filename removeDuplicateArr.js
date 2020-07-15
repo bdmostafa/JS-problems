dbd5ee45e30d64614e7ec4bed61a2151bc47d4ec
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-15 16:55:26 +0600

    Remove duplicate in array

--- removeDuplicateArr.js
+++ removeDuplicateArr.js
@@ -1,1 +1,18 @@
-let numbers = [11, 25, 64, 78, 45, 45, 11, 23, 89, 45, 62, 10, 5, 9, 7, 5];
+// Remove duplicate numbers in an array
+let numbers = [11, 78, 45, 45, 11, 45, 10, 5, 9, 5];
+let uniqueNum = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    let element = numbers[i];
+    let idx = uniqueNum.indexOf(element);
+    if (idx == -1) {
+        uniqueNum.push(element);
+    }
+}
+
+console.log(uniqueNum);
+
+// Tips ======================
+console.log(numbers.indexOf(45)); // 2
+console.log(numbers.indexOf(19)); // -1
+console.log(numbers.indexOf(11)); // 0
