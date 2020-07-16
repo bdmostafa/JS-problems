84c08f38d64845a7b1898636040d3c288849245a
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-16 22:44:45 +0600

    requirements listed

--- assignment.js
+++ assignment.js
@@ -1,15 +1,26 @@
 function feetToMile() {
-
+    // feet to mile
+    // -feet not allowed
+    // output - mile
 }
 
 function woodCalculator() {
-
+    // 1 chair - 1 cubic feet wood
+    // 1 table - 3 cubic feet wood
+    // 1 bed - 5 cubic feet wood
+    // - minus not allowed
+    // output - cubic feet
 }
 
 function brickCalculator() {
-
+    // count buliding tola
+    // 1-10 tola 15 feet height
+    // 11-20 tola 12 feet height
+    // 20- tola 10 feet height
+    // every feet need 1000 bricks constantly
+    // output - bricks numbers
 }
 
 function tinyFriend() {
-
+    // find out the lowest element from an array of friend lists
 }
