81f0494dc4f76d09b75d8ca182eefc9ff4364a6d
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-16 23:25:41 +0600

    requirements edited

--- assignment.js
+++ assignment.js
@@ -1,26 +1,48 @@
+/* 1. Feet-To-Mile converter ==========================
+Requirements:
+    input - feet,
+    negative feet not allowed,
+    convert feet to mile,
+    output - mile
+ */
 function feetToMile() {
-    // feet to mile
-    // -feet not allowed
-    // output - mile
+
 }
 
+/* 2. Wood Calculator in chair, table and bed ==================================
+Requirements:
+    input - chair, table and bed,
+    1 chair = 1 cubic feet,
+    1 table = 3 cubic feet,
+    1 bed = 5 cubic feet,
+    negative value not allowed,
+    output - cubic feet
+*/
 function woodCalculator() {
-    // 1 chair - 1 cubic feet wood
-    // 1 table - 3 cubic feet wood
-    // 1 bed - 5 cubic feet wood
-    // - minus not allowed
-    // output - cubic feet
+
 }
 
+/* 3. Brick Calculator in building ==============================
+Requirements:
+    input - building floors,
+    negative floor not allowed,
+    count floors & compare,
+    (1 - 10) floors height - 15 feet,
+    (11 - 20) floors height - 12 feet,
+    (21 - ~) floors height - 10 feet,
+    every feet need 1000 bricks constantly,
+    output - bricks number
+*/
 function brickCalculator() {
-    // count buliding tola
-    // 1-10 tola 15 feet height
-    // 11-20 tola 12 feet height
-    // 20- tola 10 feet height
-    // every feet need 1000 bricks constantly
-    // output - bricks numbers
+
 }
 
+/* 4. Find the lowest friend from an array ================================
+Requirements:
+    input - an array of friends' list,
+    empty array not allowed,
+    output - the lowest friend
+*/
 function tinyFriend() {
-    // find out the lowest element from an array of friend lists
+
 }
