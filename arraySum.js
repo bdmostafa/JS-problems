6ecd7e0e41b29b210378b2d1e9f114819e2121c0
Mostafa <mdmostafaalmahmud@gmail.com>
2020-07-18 21:10:45 +0600

    another example added using for of loop

--- arraySum.js
+++ arraySum.js
@@ -1,3 +1,4 @@
+// Using for loop ============================================
 let numbers = [85, 78, 12, 6, 45, 58, 99, 33, 56];
 
 function arraySum(numbers) {
@@ -14,7 +15,25 @@ function arraySum(numbers) {
 const result = arraySum(numbers);
 console.log('The summation of the numbers: ', result);
 
-// Another way to sum with reduce function
+
+// Using for of loop ============================================
+function sumArray(arr) {
+    let sum = 0;
+    if (!Array.isArray(arr)) return `${arr} is not an array. Please input an array!`
+
+    for (let num of arr) {
+        // let sum = 0; It should not assign here because of local scope in loop
+        sum += num;
+        // return sum; 
+    }
+    return sum;
+}
+
+const arr = [5, 6, 7];
+console.log(sumArray(arr));
+
+
+// Another way to sum with reduce function =================================
 let sum = numbers.reduce(function (a, b, ) {
     return a + b;
 }, 0); // 0 is the initial value for addition and 1 should be when its multiplication
